0296601357bb1c4867e0ab73d8c696ec90a9ab24
ln.blohin <ln.blohin@tensor.ru>
2015-10-15 20:48:56 +0300

    Roll, Pitch, Yaw

--- picam.py
+++ picam.py
@@ -4,80 +4,83 @@ import atexit
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-# This function maps the angle we want to move the servo to, to the needed PWM value
 def angleMap(angle):
 	return int((round((1950.0/180.0),0)*angle) +550)
 
-# Create a dictionary called pins to store the pin number, name, and angle
 pins = {
-    23 : {'name' : 'pan', 'angle' : 90},
-    22 : {'name' : 'tilt', 'angle' : 90}
+    23 : {'name' : 'roll', 'angle' : 90},
+    22 : {'name' : 'pitch', 'angle' : 90}
+    21 : {'name' : 'yaw', 'angle' : 90}
     }
 
-# Create two servo objects using the RPIO PWM library
-servoPan = PWM.Servo()
-servoTilt = PWM.Servo()
+servoRoll = PWM.Servo()
+servoPitch = PWM.Servo()
+servoYaw = PWM.Servo()
 
-# Setup the two servos and turn both to 90 degrees
-servoPan.set_servo(23, angleMap(90))
-servoPan.set_servo(22, angleMap(90))
+servoRoll.set_servo(23, angleMap(90))
+servoPitch.set_servo(22, angleMap(90))
+servoYaw.set_servo(21, angleMap(90))
 
-# Cleanup any open objects
 def cleanup():
     servo.stop_servo(23)
     servo.stop_servo(22)
+    servo.stop_servo(21)
 
-# Load the main form template on webrequest for the root page
 @app.route("/")
 def main():
 
-    # Create a template data dictionary to send any data to the template
     templateData = {
         'title' : 'PiCam'
         }
-    # Pass the template data into the template picam.html and return it to the user
     return render_template('picam.html', **templateData)
 
-# The function below is executed when someone requests a URL with a move direction
 @app.route("/<direction>", methods=['GET', 'POST'])
 def move(direction):
-    # Choose the direction of the request
     if direction == 'left':
-	    # Increment the angle by 10 degrees
-        na = pins[23]['angle'] + 1
-        # Verify that the new angle is not too great
+        na = pins[23]['angle'] + 10
         if int(na) <= 180:
-            # Change the angle of the servo
-            servoPan.set_servo(23, angleMap(na))
-            # Store the new angle in the pins dictionary
+            servorRoll.set_servo(23, angleMap(na))
             pins[23]['angle'] = na
         return str(na) + ' ' + str(angleMap(na))
     elif direction == 'right':
-        na = pins[23]['angle'] - 1
+        na = pins[23]['angle'] - 10
         if na >= 0:
-            servoPan.set_servo(23, angleMap(na))
+            servoRoll.set_servo(23, angleMap(na))
             pins[23]['angle'] = na
         return str(na) + ' ' + str(angleMap(na))
     elif direction == 'up':
-        na = pins[22]['angle'] + 1
+        na = pins[22]['angle'] + 10
         if na <= 180:
-            servoTilt.set_servo(22, angleMap(na))
+            servoPitch.set_servo(22, angleMap(na))
             pins[22]['angle'] = na
         return str(na) + ' ' + str(angleMap(na))
     elif direction == 'down':
-        na = pins[22]['angle'] - 1
+        na = pins[22]['angle'] - 10
         if na >= 0:
-            servoTilt.set_servo(22, angleMap(na))
+            servoPitch.set_servo(22, angleMap(na))
             pins[22]['angle'] = na
         return str(na) + ' ' + str(angleMap(na))
+    elif direction == 'spin':
+        na = pins[21]['angle'] + 10
+        if na <= 180:
+            servoYaw.set_servo(22, angleMap(na))
+            pins[21]['angle'] = na
+        return str(na) + ' ' + str(angleMap(na))
+    elif direction == 'counterspin':
+        na = pins[21]['angle'] - 10
+        if na >= 0:
+            servoYaw.set_servo(21, angleMap(na))
+            pins[21]['angle'] = na
+        return str(na) + ' ' + str(angleMap(na))
 
-# Function to manually set a motor to a specific pluse width
-@app.route("/<motor>/<pulsewidth>")
+@app.route("/<motor>/<pulsewidth>", methods=['GET', 'POST'])
 def manual(motor,pulsewidth):
-    if motor == "pan":
-        servoPan.set_servo(23, int(pulsewidth))
-    elif motor == "tilt":
-        servoTilt.set_servo(22, int(pulsewidth))
+    if motor == "roll":
+        servoRoll.set_servo(23, int(pulsewidth))
+    elif motor == "pitch":
+        servoPitch.set_servo(22, int(pulsewidth))
+    elif motor == "yaw":
+        servoYaw.set_servo(21, int(pulsewidth))
     return "Moved"
 
 # Clean everything up when the app exits
